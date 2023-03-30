c5b316a6cd72e00c97a65a82e163aa594c3249c3
saykhar7 <91285631+saykhar7@users.noreply.github.com>
2023-03-30 11:59:07 -0700

    text view added

--- app/src/main/java/com/miniboss/classrank/MainActivity.java
+++ app/src/main/java/com/miniboss/classrank/MainActivity.java
@@ -3,8 +3,24 @@ package com.miniboss.classrank;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView email = (TextView) findViewById(R.id.email);
+    TextView password = (TextView) findViewById(R.id.password);
+
+    Button login = (Button) findViewById(R.id.signInButton);
+
+    public void click(View view)
+    {
+
+        Log.i("Info", "Sign in Button Clicked");
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
