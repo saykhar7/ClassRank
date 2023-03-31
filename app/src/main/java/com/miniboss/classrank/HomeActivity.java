b5717bb79bf15f87a7d3d2742cb7c3930ea586ab
Nabeen <itsmemrnabingtm@gmail.com>
2023-03-30 18:13:27 -0700

    login, register and persistant session

--- app/src/main/java/com/miniboss/classrank/HomeActivity.java
+++ app/src/main/java/com/miniboss/classrank/HomeActivity.java
@@ -2,20 +2,24 @@ package com.miniboss.classrank;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class HomeActivity extends AppCompatActivity {
 
     private FirebaseFirestore mFirestore;
+    private FirebaseAuth mAuth;
     private TextView firstNameText, lastNameText, emailText;
+    private Button logoutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,31 +29,37 @@ public class HomeActivity extends AppCompatActivity {
         firstNameText = findViewById(R.id.firstNameText);
         lastNameText = findViewById(R.id.lastNameText);
         emailText = findViewById(R.id.emailText);
+        logoutButton = findViewById(R.id.logoutButton);
 
         mFirestore = FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
 
         String userId = getIntent().getStringExtra("user_id");
 
         mFirestore.collection("Users").document(userId)
                 .get()
-                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-//                    @Override
-                    public void onSuccess(DocumentSnapshot documentSnapshot) {
-                        String firstName = documentSnapshot.getString("firstName");
-                        String lastName = documentSnapshot.getString("lastName");
-                        String email = documentSnapshot.getString("email");
+                .addOnSuccessListener(documentSnapshot -> {
+                    String firstName = documentSnapshot.getString("firstName");
+                    String lastName = documentSnapshot.getString("lastName");
+                    String email = documentSnapshot.getString("email");
 
-                        firstNameText.setText(firstName);
-                        lastNameText.setText(lastName);
-                        emailText.setText(email);
-                    }
+                    firstNameText.setText(firstName);
+                    lastNameText.setText(lastName);
+                    emailText.setText(email);
                 })
-                .addOnFailureListener(new OnFailureListener() {
-//                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.e("HomeActivity", "Error fetching user data from Firestore", e);
-                        Toast.makeText(HomeActivity.this, "Failed to fetch user data. Please try again.", Toast.LENGTH_SHORT).show();
-                    }
+                .addOnFailureListener(e -> {
+                    Log.e("HomeActivity", "Error fetching user data from Firestore", e);
+                    Toast.makeText(HomeActivity.this, "Failed to fetch user data. Please try again.", Toast.LENGTH_SHORT).show();
                 });
+
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+                Intent intent = new Intent(HomeActivity.this, SignInActivity.class);
+                startActivity(intent);
+                finish(); // This prevents the user from pressing back and returning to the HomeActivity after logging out
+            }
+        });
     }
 }
--- app/src/main/java/com/miniboss/classrank/SignUpActivity.java
+++ app/src/main/java/com/miniboss/classrank/SignUpActivity.java
@@ -3,19 +3,15 @@ package com.miniboss.classrank;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 import java.util.HashMap;
 import java.util.Map;
-import androidx.annotation.NonNull;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.firestore.DocumentReference;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -23,6 +19,7 @@ public class SignUpActivity extends AppCompatActivity {
     private Button signUpButton;
     private TextView loginLink;
     private FirebaseFirestore mFirestore;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +27,7 @@ public class SignUpActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sign_up);
 
         mFirestore = FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
 
         firstNameEdit = findViewById(R.id.firstName);
         lastNameEdit = findViewById(R.id.lastName);
@@ -66,33 +64,40 @@ public class SignUpActivity extends AppCompatActivity {
     }
 
     private void signUpUser(String firstName, String lastName, String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        String userId = mAuth.getCurrentUser().getUid();
+                        saveUserDataToFirestore(userId, firstName, lastName, email);
+                    } else {
+                        if (task.getException() != null) {
+                            String errorMessage = task.getException().getMessage();
+                            Toast.makeText(SignUpActivity.this, "Error: " + errorMessage, Toast.LENGTH_LONG).show();
+                        } else {
+                            Toast.makeText(SignUpActivity.this, "Error creating a new user", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void saveUserDataToFirestore(String userId, String firstName, String lastName, String email) {
         Map<String, Object> user = new HashMap<>();
         user.put("firstName", firstName);
         user.put("lastName", lastName);
         user.put("email", email);
-        user.put("password", password);
 
         mFirestore.collection("Users")
-                .add(user)
-                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                    @Override
-                    public void onSuccess(DocumentReference documentReference) {
-                        String userId = documentReference.getId();
-                        Log.d("SignUpActivity", "User data set in Firestore, userId: " + userId);
+                .document(userId)
+                .set(user)
+                .addOnSuccessListener(aVoid -> {
+                    Toast.makeText(SignUpActivity.this, "User signed up successfully!", Toast.LENGTH_SHORT).show();
 
-                        Toast.makeText(SignUpActivity.this, "User signed up successfully!", Toast.LENGTH_SHORT).show();
-
-                        Intent intent = new Intent(SignUpActivity.this, HomeActivity.class);
-                        intent.putExtra("user_id", userId);
-                        startActivity(intent);
-                    }
+                    Intent intent = new Intent(SignUpActivity.this, HomeActivity.class);
+                    intent.putExtra("user_id", userId);
+                    startActivity(intent);
                 })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.e("SignUpActivity", "Error adding user data to Firestore", e);
-                        Toast.makeText(SignUpActivity.this, "Failed to sign up. Please try again.", Toast.LENGTH_SHORT).show();
-                    }
+                .addOnFailureListener(e -> {
+                    Toast.makeText(SignUpActivity.this, "Failed to save user data. Please try again.", Toast.LENGTH_SHORT).show();
                 });
     }
 }
