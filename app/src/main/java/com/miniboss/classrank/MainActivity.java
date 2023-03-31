6e2b98faca833405bf59756693450b1bf0aa5edf
Nabeen <itsmemrnabingtm@gmail.com>
2023-03-30 22:37:44 -0700

    bug fixes

--- app/src/main/java/com/miniboss/classrank/MainActivity.java
+++ app/src/main/java/com/miniboss/classrank/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -17,13 +18,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (getActionBar() != null) {
-            getActionBar().hide();
-        }
-
         FirebaseApp.initializeApp(this);
 
         Button signInButton = findViewById(R.id.signInButton);
+        Button signUpButton = findViewById(R.id.signUpButton);
+
         signInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -32,7 +31,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Button signUpButton = findViewById(R.id.signUpButton);
         signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -44,8 +42,9 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onStart() {
+        setMainContentVisibility(View.INVISIBLE);
+        setProgressBarVisibility(View.VISIBLE);
         super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseAuth.getInstance().addAuthStateListener(new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
@@ -55,8 +54,20 @@ public class MainActivity extends AppCompatActivity {
                     intent.putExtra("user_id", currentUser.getUid());
                     startActivity(intent);
                     finish(); // To prevent returning to the MainActivity when pressing the back button
+                } else {
+                    setMainContentVisibility(View.VISIBLE);
+                    setProgressBarVisibility(View.GONE);
                 }
             }
         });
+    }
+
+    private void setMainContentVisibility(int visibility) {
+        findViewById(R.id.main_content).setVisibility(visibility);
+    }
+
+    private void setProgressBarVisibility(int visibility) {
+        ProgressBar progressBar = findViewById(R.id.loading_progress_bar);
+        progressBar.setVisibility(visibility);
     }
 }
