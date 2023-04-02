fdd2d45e15f5fb8ade0ac23e3904950bba954c8b
saykhar7 <Shekhar.Basnet01@student.csulb.edu>
2023-04-02 12:29:42 -0700

    list view of classes

--- app/src/main/java/com/miniboss/classrank/HomeActivity.java
+++ app/src/main/java/com/miniboss/classrank/HomeActivity.java
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import androidx.annotation.NonNull;
 import android.os.Bundle;
 import android.content.Intent;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
@@ -21,6 +23,34 @@ public class HomeActivity extends AppCompatActivity implements HomeFragment.Home
     private ViewPager2 viewPager;
     private ViewPagerAdapter viewPagerAdapter;
 
+    String[] courses = {"Accountancy (ACCT)", "Africana Studies (AFRS)", "Am Sign Lang Ling&Deaf Culture (ASLD)",
+            "American Indian Studies (AIS)", "American Studies (AMST)", "Anthropology (ANTH)", "Arabic (ARAB)",
+            "Art (ART)", "Art History (AH)", "Asian & Asian American Studies (AAAS)", "Asian American Studies (ASAM)",
+            "Asian Studies (A/ST)", "Astronomy (ASTR)", "Athletic Training (AT)", "Athletic Training Education Pg (ATEP)",
+            "Biology (BIOL)", "Biomedical Engineering (BME)", "Business Law (BLAW)", "Business, College of (CBA)",
+            "CSU OnLine - LB ONLY (OLNE)", "Cambodian (KHMR)", "Chemical Engineering (CH E)",
+            "Chemistry & Biochemistry (CHEM)", "Chicano & Latino Studies (CHLS)", "Child Developmnt & Family Stds (CDFS)",
+            "Chinese (CHIN)", "Civil Engineering (C E)", "Classics (CLSC)", "Communication Studies (COMM)",
+            "Comparative World Literature (CWL)", "Computer Engr & Computer Sci (CECS)", "Concurrent Enrollment (XYZ)",
+            "Construction Engineering Mgmt (CEM)", "Consumer Affairs (CAFF)", "Counseling (COUN)",
+            "Criminology & Criminal Justice (CRJU)", "Dance (DANC)", "Design (DESN)", "Doctor of Physical Therapy (DPT)",
+            "Economics (ECON)", "Educ Leadership Doctorate (EDLD)", "Education - Curr & Instruction (EDCI)",
+            "Education - Early Childhood (EDEC)", "Education - Elementary (EDEL)", "Education - Secondary (EDSE)",
+            "Education - Single Subject (EDSS)", "Education Specialist (EDSP)", "Educational Administration (EDAD)",
+            "Educational Psychology (ED P)", "Educational Technology (ETEC)", "Electrical Engineering (E E)",
+            "Emergency Services Adm (EMER)", "Engineering (ENGR)", "Engineering Technology (E T)", "English (ENGL)",
+            "Environmental Engineering (ENV)", "Environmental Science & Policy (ES P)",
+            "Fashion Merchandising & Design (FMD)", "Filipino (FIL)", "Film and Electronic Arts (FEA)", "Finance (FIN)",
+            "Food Science (FSCI)", "French (FREN)", "Geography (GEOG)", "Geology (Pre Fall 2023) (GEOL)", "German (GERM)",
+            "Gerontology (GERN)", "Graduate Business Admin (GBA)", "Greek (GK)", "Health Care Administration (HCA)",
+            "Health Science (H SC)", "Hebrew (HEBW)", "History (HIST)", "Hospitality Management (HM)",
+            "Human Development (HDEV)", "Human Resources Management (HRM)", "Information Systems (I S)",
+            "International Education (INTL)", "International Studies (I/ST)", "Italian (ITAL)", "Japanese (JAPN)",
+            "Journalism (JOUR)", "Kinesiology (KIN)", "Korean (KOR)", "Latin (LAT)", "Liberal Arts, College of (C/LA)",
+            "Liberal Studies (L/ST)", "Linguistics (LING)", "Management (MGMT)", "Marketing (MKTG)"};
+
+            ListView listView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +63,15 @@ public class HomeActivity extends AppCompatActivity implements HomeFragment.Home
         viewPagerAdapter = new ViewPagerAdapter(this);
         viewPager.setAdapter(viewPagerAdapter);
         viewPager.setUserInputEnabled(false);
+
+        listView = findViewById(R.id.listMyCourses);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.activity_list_my_courses, R.id.courseTextView,courses );
+        listView.setAdapter(arrayAdapter);
+
+
+
+
+
 
         BottomNavigationView bottomNavigation = findViewById(R.id.bottom_navigation);
         bottomNavigation.setOnNavigationItemSelectedListener(item -> {
